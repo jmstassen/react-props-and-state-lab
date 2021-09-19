0b7486b2defa725e08321fb4b9c7d262d64cfaa8
John M. Stassen <john.stassen@gmail.com>
2021-09-19 09:47:54 -0700

    commit tree bf04be68d35390ef8702dfcdea33d72e0849eda2

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,47 @@ class App extends React.Component {
     }
   }
 
+  handleChangeType = (data) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: data
+      }
+    })
+  }
+
+  handleButtonClick = () => {
+    const baseURL = "/api/"
+
+    if (this.state.filters.type === "all") {
+      fetch(baseURL + "pets")
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          pets: data
+        })
+      })
+    } else {
+      fetch(baseURL + "pets?type=" + this.state.filters.type)
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          pets: data
+        })
+      })
+    }
+  }
+  
+  handlePetAdopt = (petId) => {
+    const updatedPets = this.state.pets.map(pet => {
+      return pet.id === petId ? { ...pet, isAdopted: true} : pet;
+    })
+
+    this.setState({
+      pets: updatedPets
+    })
+  }
+  
   render() {
     return (
       <div className="ui container">
@@ -24,10 +65,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleChangeType} onFindPetsClick={this.handleButtonClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.handlePetAdopt} />
             </div>
           </div>
         </div>
